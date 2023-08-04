fe3c6e7e7b0d650fcb2535897ea14200ae1cc5d4
Samuel Caires <133229901+SamuelCaires15@users.noreply.github.com>
2023-08-04 11:37:43 +0000

    Meu nome

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000;                  // Define a porta
 
 app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   res.json({
-    nome: "Seu_nome_completo",      // Substitua pelo seu nome
+    nome: "Samuel Caires",      // Substitua pelo seu nome
   });
   console.log("Rota / solicitada");
 });
