155903f799de609c2910c3e0215bbc8531aae9cb
Samuel Caires <133229901+SamuelCaires15@users.noreply.github.com>
2023-08-11 11:49:54 +0000

    new function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+app.use(express.json());
 import express from "express";      // Requisição do pacote do express
 import dotenv from "dotenv";
-import { selectUsuarios } from "./bd.js";
+import { selectUsuarios, selectUsuario, insertUsuario } from "./bd.js";
 dotenv.config();
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
@@ -18,6 +19,16 @@ app.get("/usuario/:id", async (req, res) => {
     const usuario = await selectUsuario(req.params.id);
     if (usuario.length > 0) res.json(usuario);
     else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
