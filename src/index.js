506c0a01411234357aa25e647fb7565908c28f0a
Samuel Caires <133229901+SamuelCaires15@users.noreply.github.com>
2023-08-31 13:18:19 +0000

    novas rotas

--- src/index.js
+++ src/index.js
@@ -1,40 +1,20 @@
-import express from "express";      // Requisição do pacote do express
-import dotenv from "dotenv";
-import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario } from "./db/bd.js";
+import roteadorUsuario from "./routes/usuario.js";
 
+app.use(roteadorUsuario);
+
+import dotenv from "dotenv";
+import express from "express";
 dotenv.config();
 
-const app = express();              // Instancia o Express
-const port = 3000;                  // Define a porta
+const app = express();
+const port = 3000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  console.log("Rota / solicitada");
-  // Cria a rota da raiz do projeto
   res.json({
-    nome: "Samuel Caires", // Substitua pelo seu nome
+    message: "API para CRUD usuario: link_gitgub",
   });
-});
-
-app.get("/usuarios", async (req, res) => {
-  try {
-    const usuarios = await selectUsuarios();
-    res.json(usuarios);
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-
-  console.log("Rota GET/usuarios solicitada");
-});
-
-app.get("/usuario/:id", async (req, res) => {
-  console.log("Rota GET /usuario solicitada");
-  try {
-    const usuario = await selectUsuario(req.params.id);
-    if (usuario.length > 0) res.json(usuario);
-    else res.status(404).json({ message: "Usuário não encontrado!" });
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
 });
 
 app.listen(port, () => {
@@ -42,42 +22,16 @@ app.listen(port, () => {
   console.log(`Serviço escutando na porta:  ${port}`);
 });
 
-app.use(express.json());
+//src/routes/usuario.js
+const router = Router();
 
-app.post("/usuario", async (req, res) => {
-  console.log("Rota POST /usuario solicitada");
+router.get("/usuario", async (req, res) => {
+  console.log(`Rota GET /usuarios solicitada pelo usuario ${req.userId}`);
   try {
-    await insertUsuario(req.body);
-    res.status(201).json({ message: "Usuário inserido com sucesso!" });
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
 });
-
-app.delete("/usuario/:id", async (req, res) => {
-  console.log("Rota DELETE /usuario solicitada");
-  try {
-    const usuario = await selectUsuario(req.params.id);
-    if (usuario.length > 0) {
-      await deleteUsuario(req.params.id);
-      res.status(200).json({ message: "Usuário excluido com sucesso!!" });
-    } else res.status(404).json({ message: "Usuário não encontrado!" });
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
-
-app.patch("/usuario", async (req, res) => {
-  console.log("Rota PATCH /usuario solicitada");
-  try {
-    const usuario = await selectUsuario(req.body.id);
-    if (usuario.length > 0) {
-      await updateUsuario(req.body);
-      res.status(200).json({ message: "Usuário atualizado com sucesso!" });
-    } else res.status(404).json({ message: "Usuário não encontrado!" });
-  } catch (error) {
-    console.log(error);
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
-
+export default router;
